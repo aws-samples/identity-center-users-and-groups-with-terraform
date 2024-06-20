3c252cfefe90e896e37b207ea61531d62fbcfdd8
neveene <neveene@amazon.co.uk>
2024-06-20 15:46:18 +0100

    modified users and groups dependency to eliminate the use of "depends on"

--- local.tf
+++ local.tf
@@ -5,6 +5,16 @@ locals {
   groups_yaml = var.groups != null ? file(var.groups) : ""
   groups_list = local.groups_yaml != "" ? yamldecode(local.groups_yaml) : {}
 
+  groups_flatten = flatten([
+    for group_name, group_details in local.groups_list : [
+      for user in group_details.users :
+      {
+        user  = user
+        group = group_name
+      }
+    ]
+  ])
+
   users_yaml = var.users != null ? file(var.users) : ""
   users_list = local.users_yaml != "" ? yamldecode(local.users_yaml) : {}
 }
--- main.tf
+++ main.tf
@@ -3,29 +3,33 @@
 
 data "aws_ssoadmin_instances" "this" {}
 
-module "groups" {
-  source            = "./modules/groups"
-  for_each          = local.groups_list
-  group_name        = each.key
-  group_description = each.value.description
-  group_membership  = toset(each.value.users)
-  store_id          = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  depends_on = [ aws_identitystore_user.this ]
-}
-
 resource "aws_identitystore_user" "this" {
-  for_each = local.users_list
+  for_each          = local.users_list
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
-  display_name = each.value.first_name
+  display_name = title("${each.value.first_name} ${each.value.last_name}")
   user_name    = each.key
 
   name {
-    given_name  = each.value.first_name
-    family_name = each.value.last_name
+    given_name  = title(each.value.first_name)
+    family_name = title(each.value.last_name)
   }
 
   emails {
-    value = each.value.email
+    value = lower(each.value.email)
   }
+}
+
+resource "aws_identitystore_group" "this" {
+  for_each          = local.groups_list
+  display_name      = title(each.key)
+  description       = each.value.description
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+}
+
+resource "aws_identitystore_group_membership" "this" {
+  for_each          = { for group in local.groups_flatten : "${group.group}-${group.user}" => group }
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+  group_id          = aws_identitystore_group.this[each.value.group].group_id
+  member_id         = aws_identitystore_user.this[each.value.user].user_id
 }
